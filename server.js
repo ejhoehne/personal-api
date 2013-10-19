b19a5ae6d27a3121c03a29f49a1fe39dafda4715
Emily <ejhoehne@gmail.com>
2013-10-19 13:08:45 -0600

    Step 4

--- server.js
+++ server.js
@@ -11,23 +11,25 @@ var information= {
 	name: [{"First":"Emily"}, {"Last":"Hoehne"}],
 	location: [{"City":"Provo"}, {"State":"Utah"}],
 	hobbies: ["Economics", "Yoga", "Cooking"],
-	occupations: ["Assistant", "Receptionist", "Manager"]
-}
+	occupations: ["Assistant", "Receptionist", "Manager"],
+	mentions:[{"Website":"http://www.facebook.com"}, {"Website":"http://www.twitter.com"}],
+	friends:[{"Name":"Ann"}, {"Name":"Becca"}, {"Name":"Joe"}]
+};
 app.get('/name', function(req,res)
 {
 	res.type('text/plain');
-  	res.send(information.name);
+	res.send(information.name);
 });
 app.get('/location', function(req,res){
 	res.type('text/plain');
-  	res.send(information.location);
+	res.send(information.location);
 
 });
 app.get('/hobbies', function(req,res){
 	res.type('text/plain');
 	res.send(information.hobbies);
 	var order= req.query.order;
-	var sortedHobbies= information.hobbies.sort();	
+var sortedHobbies= information.hobbies.sort();
 	if(order=='desc'){
 		sortedHobbies= sortedHobbies.reverse();
 	}
@@ -36,20 +38,39 @@ app.get('/hobbies', function(req,res){
  });
 app.get('/occupations', function(req,res){
 	res.type('text/plain');
-  	res.send(information.occupations);
-  	var order= req.query.order;
-	var sortedOccupations= information.occupations.sort();	
-	if(order=='desc'){
+		res.send(information.occupations);
+	var order= req.query.order;
+	var sortedOccupations= information.occupations.sort();
+	if (order=='desc'){
 		sortedOccupations= sortedOccupations.reverse();
 	}
 	res.send(sortedOccupations);
 		
  });
  app.get('/occupations/latest', function(req,res){
- 	res.type('text/plain');
- 	res.send(information.occupations[information.occupations.length-1]);
+	res.type('text/plain');
+	res.send(information.occupations[information.occupations.length-1]);
 
  });
+ app.post('/mentions', function(req,res){
+	var newPost= req.body;
+	information.mentions.push(newPost);
+	res.end();
+ });
+ app.get('/mentions', function(req,res){
+	console.log('you posted!');
+	res.send(information.mentions);
+ });
+ app.post('/friends', function(req,res){
+	var newPost= req.body;
+	information.friends.push(newPost);
+	res.end();
+ });
+ app.get('/friends', function(req,res){
+	console.log('you posted!');
+	res.send(information.friends);
+ });
+ 
 
 app.listen(8080);
 console.log('Listening on Port 8080');
