19b9bba6aef5ac3c7aa69df296438b61a8799e1e
Emily <ejhoehne@gmail.com>
2013-10-19 11:54:44 -0600

    Step 2

--- server.js
+++ server.js
@@ -7,31 +7,35 @@ app.use(function(req, res, next){
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
+var information= {
+	name: [{"First":"Emily"}, {"Last":"Hoehne"}],
+	location: [{"City":"Provo"}, {"State":"Utah"}],
+	hobbies: ["Economics", "Yoga", "Cooking"],
+	occupations: ["Assistant", "Receptionist", "Manager"]
+}
 app.get('/name', function(req,res)
 {
-	res.type('json');
-  	res.json({name:'Emily Hoehne'}); 
+	res.type('text/plain');
+  	res.send(information.name);
 });
 app.get('/location', function(req,res){
-	res.type('json');
-  	res.json({location:'Provo, Utah'}); 
+	res.type('text/plain');
+  	res.send(information.location);
 
 });
 app.get('/hobbies', function(req,res){
-	res.type('json');
-	//res.json({"obj":[{"name":"tt"},{"name":"gg"}]}); 
-  	res.json({"hobbies":['Economics', 'Gluten-free', 'Flute', 'Yoga']});
-  		
-});
+	res.type('text/plain');
+  	res.send(information.hobbies);
+ });
 app.get('/occupations', function(req,res){
-	res.type('json');
-  	res.json({occupations:['Office Manager', 'Receptionist', 'Admin Assistant']}); 
-});
-app.get('/occupations/latest', function(req,res){
-	res.type('json');
-	res.json({Latest:'Office Manager'}); 
+	res.type('text/plain');
+  	res.send(information.occupations);
+ });
+ app.get('/occupations/latest', function(req,res){
+ 	res.type('text/plain');
+ 	res.send(information.occupations[information.occupations.length-1]);
 
-});
+ });
 
 app.listen(8080);
 console.log('Listening on Port 8080');
